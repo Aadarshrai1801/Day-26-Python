2038e20561fd48b1878db4b86693d985cd9cf882
Aadarshrai1801 <aadarshrai1801@gmail.com>
2025-10-31 23:11:54 +0530

    commit

--- NATOAlphabet.py
+++ NATOAlphabet.py
@@ -6,8 +6,15 @@ data = pandas.read_csv("26thDay\Alphabets.csv")
 
 data_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 
-name = input("Enter a word: ").upper()
+def func() :
+    name = input("Enter a word: ").upper()
 
-nato_list = [data_dict[letter] for letter in name if letter in data_dict]
-    
-print(nato_list)    
+    try :
+        nato_list = [data_dict[letter] for letter in name]
+    except KeyError :
+        print("Sorry, only letters in the alphabet please!")
+        func()
+    else :
+        print(nato_list)
+        
+func()                    
